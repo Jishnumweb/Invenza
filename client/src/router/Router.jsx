eeffa00042f764591948fca7e547592f2281cccf
Jishnu <jishnu@researck.com>
2025-07-13 11:15:13 +0530

    dashboard page setup

--- client/src/router/Router.jsx
+++ client/src/router/Router.jsx
@@ -3,10 +3,15 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Signup from "../components/Signup";
+import Dashboard from "../pages/Dashboard";
 
 export const Router = createBrowserRouter([
   {
     path: "/",
     element: <Signup/>,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard/>,
   },
 ]);
